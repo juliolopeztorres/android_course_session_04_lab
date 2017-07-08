03932159486774a6a1e2834ebc7db6181939b826
Julio López Torres <juliolopeztorres@gmail.com>
2017-07-08 22:51:16 +0200

    feat: Add second activity in order to add and edit a city (Layout, icons and activity)

---
+++ app/src/main/java/oob/cityworld/activities/CityDetailsActivity.java
@@ -0,0 +1,15 @@
+package oob.cityworld.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import oob.cityworld.R;
+
+public class CityDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_details);
+    }
+}
