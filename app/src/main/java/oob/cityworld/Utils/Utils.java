f27b4a215bade23799313f4b82264051a7bb29ee
Julio López Torres <juliolopeztorres@gmail.com>
2017-07-09 12:42:14 +0200

    feat: Add Toast message method and load url to ImageView method

--- app/src/main/java/oob/cityworld/Utils/Utils.java
+++ app/src/main/java/oob/cityworld/Utils/Utils.java
@@ -1,8 +1,12 @@
 package oob.cityworld.Utils;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
+import android.widget.ImageView;
+import android.widget.Toast;
+import com.squareup.picasso.Picasso;
 
 public class Utils {
 
@@ -19,5 +23,19 @@ public class Utils {
                 .setPositiveButton(positiveActionText, positiveCallback)
                 .setNegativeButton(negativeActionText, negativeCallback)
                 .show();
+    }
+
+    public static void showToast(Context context, String message) {
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+    }
+
+    public static void checkAndLoadUrlImage(String url, Activity activity, ImageView imageView, String messageEmpty) {
+        if (url.isEmpty()) {
+            Utils.showToast(activity, messageEmpty);
+        } else if (url.contains("http://") || url.contains("https://")){
+            Picasso.with(activity).load(url).fit().into(imageView);
+        } else {
+            Utils.showToast(activity, messageEmpty);
+        }
     }
 }
